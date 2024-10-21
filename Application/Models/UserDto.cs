df9d80d11e2af282ba163e004883a8ee90a03136
Kevin Caceres <kmcaceres98@gmail.com>
2024-10-21 17:43:19 -0300

    GetAllUser

--- Application/Models/UserDto.cs
+++ Application/Models/UserDto.cs
@@ -1,4 +1,5 @@
-﻿using Domain.Enums;
+﻿using Domain.Entities;
+using Domain.Enums;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace Application.DTOs
 {
-    internal class UserDto
+    public class UserDto
     {
         public int Id { get; set; }
         public string Name { get; set; }
@@ -15,5 +16,30 @@ namespace Application.DTOs
         public string Email { get; set; }
         public string Address { get; set; }
         public string Phone { get; set; }
+
+        public static UserDto Create(User user)
+        {
+            UserDto userDto = new UserDto();
+
+            userDto.Id = user.Id;
+            userDto.Name = user.Name;
+            userDto.Role = user.Role; // Suponiendo que UserRole es un enum
+            userDto.Email = user.Email;
+            userDto.Address = user.Address;
+            userDto.Phone = user.Phone;
+
+            return userDto;
+        }
+
+        // Método para crear una lista de UserDto a partir de una colección de User
+        public static List<UserDto> CreateList(IEnumerable<User> users)
+        {
+            var listDto = new List<UserDto>();
+            foreach (var user in users)
+            {
+                listDto.Add(Create(user)); // Llama al método Create para cada usuario
+            }
+            return listDto;
+        }
     }
 }
